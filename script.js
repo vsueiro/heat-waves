165e6f81db2002079fd17c0c4e921f143a06268a
Vinicius Sueiro <hi@vsueiro.com>
2023-12-02 13:55:01 -0500

    Add basic 3D line example

---
+++ script.js
@@ -0,0 +1,115 @@
+import * as THREE from "three";
+
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { Line2 } from "three/addons/lines/Line2.js";
+import { LineMaterial } from "three/addons/lines/LineMaterial.js";
+import { LineGeometry } from "three/addons/lines/LineGeometry.js";
+import * as GeometryUtils from "three/addons/utils/GeometryUtils.js";
+
+let line, matLine, renderer, scene, camera, controls;
+
+function init() {
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setClearColor(0x000000, 1);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    40,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+  camera.position.set(-40, 0, 60);
+
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.minDistance = 10;
+  controls.maxDistance = 500;
+
+  // Position and THREE.Color Data
+
+  const positions = [];
+  const colors = [];
+
+  const points = GeometryUtils.hilbert3D(
+    new THREE.Vector3(0, 0, 0),
+    20.0,
+    1,
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7
+  );
+
+  const spline = new THREE.CatmullRomCurve3(points);
+  const divisions = Math.round(16 * points.length);
+  const point = new THREE.Vector3();
+  const color = new THREE.Color();
+
+  for (let i = 0, l = divisions; i < l; i++) {
+    const t = i / l;
+
+    spline.getPoint(t, point);
+    positions.push(point.x, point.y, point.z);
+
+    color.setHSL(t, 1.0, 0.5, THREE.SRGBColorSpace);
+    colors.push(color.r, color.g, color.b);
+  }
+
+  // Line2 ( LineGeometry, LineMaterial )
+
+  const geometry = new LineGeometry();
+  geometry.setPositions(positions);
+  geometry.setColors(colors);
+
+  matLine = new LineMaterial({
+    color: 0xffffff,
+    linewidth: 20, // in world units with size attenuation, pixels otherwise
+    vertexColors: true,
+  });
+
+  line = new Line2(geometry, matLine);
+  line.computeLineDistances();
+  line.scale.set(1, 1, 1);
+  scene.add(line);
+
+  window.addEventListener("resize", onWindowResize);
+  onWindowResize();
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  // main scene
+
+  renderer.setClearColor(0x000000, 1);
+
+  renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
+
+  controls.update();
+
+  // renderer will set this eventually
+  matLine.resolution.set(window.innerWidth, window.innerHeight); // resolution of the viewport
+
+  renderer.render(scene, camera);
+
+  renderer.setScissorTest(false);
+}
+
+init();
+animate();
